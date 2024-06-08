684cc750276301b65574e987160f279e585c8d26
LazarenkoNatalia <lazarenkon.n.v@gmail.com>
2024-06-08 17:42:47 +0300

    dz5_initversel

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import { Route, Routes } from 'react-router-dom'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+ 
+//   const options = {
+//     method: 'GET', headers: {
+//       accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYzM2MGQyYThlMzdiMWFjMDNkZThhY2FmM2FlMjA0YyIsInN1YiI6IjY2NjQ1NjM1ZjcxM2ZlODE1YzZkZGZhOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P2UylFvV-QPamRqHUoX2bAmnXn_G2RdTK2VLTcMJRPQ'
+//  //  API_KEY: 'cc360d2a8e37b1ac03de8acaf3ae204c'
+// }};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+//   return 
+//   <>
+//     <p>params</p>
+//   </>
+
 }
 
-export default App
+
